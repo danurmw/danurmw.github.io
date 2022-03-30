b7305c5673ca6d71b4ce2c4385c1205bac3d7cf5
Danur Arkan <danurarkan@gmail.com>
2022-03-31 00:23:51 +0700

    second commit

--- script.js
+++ script.js
@@ -1,4 +1,98 @@
-// const numbers = document.querySelectorAll(".number")
-// console.log(numbers);
+const calculatorScreen = document.querySelector(`.calculator-screen`);
 
-console.log("adad");
+const updatescreen = (number) => {
+  calculatorScreen.value = number;
+};
+
+const numbers = document.querySelectorAll(".number");
+
+const inputNumber = (number) => {
+  if (currentNumber === "0") {
+    currentNumber = number;
+  } else {
+    currentNumber += number;
+  }
+};
+
+numbers.forEach((number) => {
+  number.addEventListener("click", (event) => {
+    inputNumber(event.target.value);
+    updatescreen(currentNumber);
+  });
+});
+
+let prevNumber = "";
+let calculationOperator = "";
+let currentNumber = "0";
+
+const operators = document.querySelectorAll(".operator");
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", (event1) => {
+    inputOperator(event1.target.value);
+  });
+});
+
+const inputOperator = (operator) => {
+  if (calculationOperator === "") {
+    prevNumber = currentNumber;
+  }
+  calculationOperator = operator;
+  currentNumber = "0";
+};
+
+const equalSign = document.querySelector(".equal-sign");
+
+equalSign.addEventListener("click", () => {
+  calculate();
+  updatescreen(currentNumber);
+});
+
+const calculate = () => {
+  let result = "";
+  switch (calculationOperator) {
+    case "+":
+      result = parseFloat(prevNumber) + parseFloat(currentNumber);
+      break;
+    case "-":
+      result = prevNumber - currentNumber;
+      break;
+    case "*":
+      result = prevNumber * currentNumber;
+      break;
+    case "/":
+      result = prevNumber / currentNumber;
+      break;
+    default:
+      return;
+  }
+  currentNumber = result;
+  calculationOperator = "";
+};
+
+const clearBtn = document.querySelector(".all-clear");
+
+clearBtn.addEventListener("click", () => {
+  clearAll();
+  updatescreen(currentNumber);
+});
+
+const clearAll = () => {
+  prevNumber = "";
+  calculationOperator = "";
+  currentNumber = "0";
+};
+
+const decimal = document.querySelector(".decimal");
+
+decimal.addEventListener("click", (event2) => {
+  inputDecimal(event2.target.value);
+  updatescreen(currentNumber);
+});
+
+inputDecimal = (dot) => {
+  if (currentNumber.includes(".")) {
+    return;
+  }
+  currentNumber += dot;
+};
